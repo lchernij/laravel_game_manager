8d2be43b9b71583f6df0856a486cfc7f9441d690
Leandro Chernij <lchernij@gmail.com>
2021-07-17 21:55:35 -0300

    Add Games work

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('games/{id}/restore', [GameController::class, "restore"])->name('games.restore');
+Route::apiResource('games', GameController::class);
